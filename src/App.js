4af284785dbfae6da572507cc145366feffdb30b
宇治 護 <sugoiyodarekasan@gmail.com>
2020-09-12 16:35:01 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 const App = props => {
-  const [state, setState] = useState(props);
-  const { name, price } = state;
+  const [state, setState] = useState(props)
+  const { name, price } = state
+
+  useEffect(() => {
+    console.log('This is like componentDimMount or componentDidUpdate')
+  })
+
+  useEffect(() => {
+    console.log('This is like componentDimMount')
+  }, [])
+
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
 
   return (
     <>
       <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button >
-      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button >
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
       <button onClick={() => setState(props)}>reset</button>
       <input value={name} onChange={e => setState({ ...state, name: e.target.value })} />
     </>
@@ -20,4 +32,4 @@ App.defaultProps = {
   price: 1000
 }
 
-export default App;
+export default App
